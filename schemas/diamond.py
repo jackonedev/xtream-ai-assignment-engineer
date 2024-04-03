cb15230c4a59129bde7b309925d947f4ef2ed21b
jackonedev <jackone.action.software@gmail.com>
2024-04-03 15:16:17 -0300

    created enum parameters for Diamond schema validation

--- schemas/diamond.py
+++ schemas/diamond.py
@@ -1,10 +1,39 @@
+from enum import Enum
+
 from pydantic import BaseModel, ConfigDict
+
+
+class Cut(str, Enum):
+    IDEAL = 'Ideal'
+    PREMIUM = 'Premium'
+    VERY_GOOD = 'Very Good'
+    GOOD = 'Good'
+    FAIR = 'Fair'
+
+class Color(str, Enum):
+    D = 'D'
+    E = 'E'
+    F = 'F'
+    G = 'G'
+    H = 'H'
+    I = 'I'
+    J = 'J'
+
+class Clarity(str, Enum):
+    IF = 'IF'
+    VVS1 = 'VVS1'
+    VVS2 = 'VVS2'
+    VS1 = 'VS1'
+    VS2 = 'VS2'
+    SI1 = 'SI1'
+    SI2 = 'SI2'
+    I1 = 'I1'
 
 class Diamond(BaseModel):
     carat: float
-    cut: str
-    color: str
-    clarity: str
+    cut: Cut
+    color: Color
+    clarity: Clarity
     depth: float
     table: float
     x: float
