742beec857dcd8c88fd269793c61b1d52a3028a3
jackonedev <jackone.action.software@gmail.com>
2024-04-03 12:33:37 -0300

    updated system files

--- utils/config.py
+++ utils/config.py
@@ -5,8 +5,8 @@ PROJECT_ROOT = os.path.dirname('.')
 DATASET_PATH = os.path.join(PROJECT_ROOT, 'datasets', 'diamonds', 'diamonds.csv')
 SEED = 33
 
-os.makedirs(os.path.join(PROJECT_ROOT, 'models'), exist_ok=True)
-MODELS_PATH = os.path.join(PROJECT_ROOT, 'models')
+os.makedirs(os.path.join(PROJECT_ROOT, '.models'), exist_ok=True)
+MODELS_PATH = os.path.join(PROJECT_ROOT, '.models')
 
 os.makedirs(os.path.join(MODELS_PATH, 'data'), exist_ok=True)
 MODEL_DATA_PATH = os.path.join(MODELS_PATH, 'data')
