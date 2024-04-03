bfe9b14b4e7643a0ff02442424fd10a7a527ca7e
jackonedev <jackone.action.software@gmail.com>
2024-04-03 15:16:40 -0300

    updated utils/config.py module

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,11 @@
 import os
 
-PROJECT_ROOT = os.path.dirname('.')
-
-DATASET_PATH = os.path.join(PROJECT_ROOT, 'datasets', 'diamonds', 'diamonds.csv')
 SEED = 33
+PROJECT_ROOT = os.path.dirname('.')
+DATASET_ROOT = os.path.join(PROJECT_ROOT, 'datasets')
+
+DATASET_PATH = os.path.join(DATASET_ROOT, 'diamonds', 'diamonds.csv')
+DATASET_INFO_PATH = os.path.join(DATASET_ROOT, 'diamonds', 'README.md')
 
 os.makedirs(os.path.join(PROJECT_ROOT, '.models'), exist_ok=True)
 MODELS_PATH = os.path.join(PROJECT_ROOT, '.models')
