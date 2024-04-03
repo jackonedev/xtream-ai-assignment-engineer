4a28ed20c2d1d7ac4f8b83753979b6878e770321
jackonedev <jackone.action.software@gmail.com>
2024-04-03 16:16:39 -0300

    updated models endpoint

--- api/models.py
+++ api/models.py
@@ -1,21 +1,23 @@
 from typing import Annotated
 
+from pydantic import ValidationError
 from fastapi import (
     APIRouter, Form,
-    status, HTTPException, 
-    Depends
+    status, HTTPException
 )
+
+from ml_injection_pipeline import prediction_pipeline
 from schemas.diamond import (
     Diamond,
     Cut, Color, Clarity
-    )
-
+)
 
 
 router = APIRouter(
     prefix="/models",
     tags=["Diamond Model"]
 )
+
 
 @router.post("/predictions/prices", status_code=status.HTTP_200_OK)
 async def predict_price(
@@ -28,7 +30,34 @@ async def predict_price(
     x: Annotated[float, Form()],
     y: Annotated[float, Form()],
     z: Annotated[float, Form()]
-    ):
-    return {"price": 100}
+):
 
-    
+    form = {
+        "carat": carat,
+        "cut": cut,
+        "color": color,
+        "clarity": clarity,
+        "depth": depth,
+        "table": table,
+        "x": x,
+        "y": y,
+        "z": z
+    }
+
+    try:
+        Diamond(**form)
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=e.errors()
+        )
+
+    try:
+        price = prediction_pipeline(form)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+    return {"price": price}
