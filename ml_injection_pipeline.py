85a0eff224331cc8504852ba92aae071426c8e83
jackonedev <jackone.action.software@gmail.com>
2024-04-05 17:47:27 -0300

    Added single pipeline for each category

--- ml_injection_pipeline.py
+++ ml_injection_pipeline.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable
 import os
 import pickle
 import datetime as dt
@@ -7,14 +7,20 @@ import numpy as np
 import pandas as pd
 from pydantic import ValidationError
 
-from schemas.diamond import Diamond
+from schemas.diamond import Diamond, DiamondDataFrame
 from utils.config import MODELS_PATH, DATASET_PATH
 
 
 # LOAD THE MODELS AND TRANSFORM PIPELINE
 
-with open(os.path.join(MODELS_PATH, "transform_pipeline.pkl"), "rb") as file:
-    transform_pipeline = pickle.load(file)
+with open(os.path.join(MODELS_PATH, "log_reg_pipeline.pkl"), "rb") as file:
+    log_reg_pipeline = pickle.load(file)
+
+with open(os.path.join(MODELS_PATH, "common_pipeline.pkl"), "rb") as file:
+    common_pipeline = pickle.load(file)
+
+with open(os.path.join(MODELS_PATH, "exclusive_pipeline.pkl"), "rb") as file:
+    exclusive_pipeline = pickle.load(file)
 
 with open(os.path.join(MODELS_PATH, "log_reg.pkl"), "rb") as file:
     log_reg = pickle.load(file)
@@ -27,10 +33,17 @@ with open(os.path.join(MODELS_PATH, "lgbm_exclusive.pkl"), "rb") as file:
 
 
 def data_transformation(data: Any) -> pd.DataFrame:
+    cols = pd.read_csv(DATASET_PATH).drop(columns='price').columns
 
     if isinstance(data, dict):
         try:
-            Diamond(**data)
+            # Validation block
+            try:
+                Diamond(**data)
+            except:
+                conv = pd.DataFrame(data).to_dict(orient='records')
+                DiamondDataFrame(data=conv)
+            
         except ValidationError as e:
             print("Error transforming the input:")
             for element in e.errors():
@@ -42,11 +55,12 @@ def data_transformation(data: Any) -> pd.DataFrame:
         data = pd.DataFrame(data, index=[0])
 
     elif isinstance(data, type(np.array(0))) or isinstance(data, list):
-        cols = pd.read_csv(DATASET_PATH).drop(columns='price').columns
         data = {col: [val] for col, val in zip(cols, data)}
         data = pd.DataFrame(data)
+        
+    elif isinstance(data, pd.Series):
+        data = data.to_frame().T
 
-    data = transform_pipeline.transform(data)
     return data
 
 
@@ -64,24 +78,48 @@ def prediction_pipeline(data: Any) -> float:
     cat_threshold = 0.92
     avg_infl_rate = 0.04
     delta_year = dt.datetime.now().year - 2008
-    data = data_transformation(data)
+    
+    _data = data_transformation(data)
+    _X = log_reg_pipeline.transform(_data)
 
-    if len(data) == 1:
-        cat = log_reg.predict_proba(data)[:, 1]
+    if len(_X) == 1:
+        # Single prediction
+        cat = log_reg.predict_proba(_X)[:, 1]
         if cat > cat_threshold:
             cat = 'Exclusive'
-            pred = lgbm_exclusive.predict(data)[0]
+            _X = exclusive_pipeline.transform(_data)
+            pred = lgbm_exclusive.predict(_X)[0]
         else:
             cat = 'Common'
-            pred = lgbm_common.predict(data)[0]
+            _X = common_pipeline.transform(_data)
+            pred = lgbm_common.predict(_X)[0]
             pred = pred * (1 + avg_infl_rate) ** delta_year
         return round(pred, 1)
 
     else:
-        cat = log_reg.predict_proba(data)[:, 1]
+        # Batch prediction
+        cat = log_reg.predict_proba(_X)[:, 1]
         cat = np.where(cat > cat_threshold, 'Exclusive', 'Common')
-        pred = np.where(cat == 'Exclusive', lgbm_exclusive.predict(
-            data), lgbm_common.predict(data))
-        pred = np.where(cat == 'Common', pred *
-                        (1 + avg_infl_rate) ** delta_year, pred)
+        
+        original_order = {}
+        for i, z in enumerate(zip(_data.index, cat)):
+            original_order[i] = tuple(z)
+            
+        agrupated_index = {'Exclusive': [], 'Common': []}
+        for oo in original_order.values():
+            if oo[1] == 'Exclusive':
+                agrupated_index['Exclusive'].append(oo[0])
+            else:
+                agrupated_index['Common'].append(oo[0])
+                
+        _Xe = exclusive_pipeline.transform(_data.loc[agrupated_index['Exclusive']])
+        _Xc = common_pipeline.transform(_data.loc[agrupated_index['Common']])
+
+        pred_exclusive = lgbm_exclusive.predict(_Xe)
+        pred_common = lgbm_common.predict(_Xc)
+        pred_common = pred_common * (1 + avg_infl_rate) ** delta_year
+                
+        pred = np.concatenate([pred_exclusive, pred_common])
+        pred = pred[np.argsort(list(original_order.keys()))]
+        
         return pred.round(1)
